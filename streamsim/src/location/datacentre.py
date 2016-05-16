4d3b10b6a867b387df4ff5b8d55605258f238692
Richard G. Clegg <richard@richardclegg.org>
2016-05-16 14:40:06 +0100

    Add new model for instance costs

--- streamsim/src/location/datacentre.py
+++ streamsim/src/location/datacentre.py
@@ -6,7 +6,8 @@ import locationProvider
 class datacentre(locationProvider.locationProvider):
     """Class representing a data center at a given location"""
 
-    def __init__(self, loc, inCost,outCost):
+    def __init__(self, loc, inCost,outCost,instcost=0):
         locationProvider.locationProvider.__init__(self,loc)
         self.inCost= inCost
         self.outCost= outCost
+        self.instCost=instcost
