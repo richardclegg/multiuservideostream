0b75723aa1ed9ad55ceefe07226b1056d40bf060
Richard G. Clegg <richard@richardclegg.org>
2016-06-21 23:01:41 +0100

    Scripts to vary number of users

---
+++ streamsim/toccscripts/poker_var_user.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# Run from directory ABOVE scripts
+
+OUT_FILE="data/poker_var_user_ndynamic.out"
+IN_FILE="scripts/poker_var_user_ndynamic.json"
+SCRIPT=scripts/meansd.py
+RND=$RANDOM
+TMPJSONFILE="/tmp/poker_var_$RND.json"
+TMPCOSTFILE="/tmp/poker_var_cost_$RND.out"
+TMPDAYFILE="/tmp/poker_var_day_$RND.out"
+TMPSESSFILE="/tmp/poker_var_sess_$RND.out"
+TMPQOSFILE="/tmp/poker_var_qos_$RND.out"
+
+rm -f $OUT_FILE
+for users in "2500" "5000" "7500" "10000"; do
+    sed -e "s/XXXXXX/$users/g" $IN_FILE | sed -e "s%DAYTMPFILE%$TMPDAYFILE%g" | sed -e "s%COSTTMPFILE%$TMPCOSTFILE%g" | sed -e "s%QOSTMPFILE%$TMPQOSFILE%g" | sed -e "s%SESSTMPFILE%$TMPSESSFILE%g"> $TMPJSONFILE
+    src/streamsim.py $TMPJSONFILE
+    echo -n $users "" >> $OUT_FILE
+    cat $TMPCOSTFILE | $SCRIPT | tr -d '\n' >> $OUT_FILE
+    echo -n " " >> $OUT_FILE
+    cat $TMPQOSFILE | $SCRIPT | tr -d '\n'>> $OUT_FILE
+    echo -n " " >> $OUT_FILE
+    cat $TMPSESSFILE | $SCRIPT | tr -d '\n'>> $OUT_FILE
+    echo >> $OUT_FILE
+rm -f $TMPJSONFILE $TMPCOSTFILE $TMPDAYFILE $TMPQOSFILE    
+done
+
+OUT_FILE="data/poker_var_user_nstatic.out"
+IN_FILE="scripts/poker_var_user_nstatic.json"
+rm -f $OUT_FILE
+for users in "2500" "5000" "7500" "10000"; do
+    sed -e "s/XXXXXX/$users/g" $IN_FILE | sed -e "s%DAYTMPFILE%$TMPDAYFILE%g" | sed -e "s%COSTTMPFILE%$TMPCOSTFILE%g" | sed -e "s%QOSTMPFILE%$TMPQOSFILE%g" | sed -e "s%SESSTMPFILE%$TMPSESSFILE%g"> $TMPJSONFILE
+    src/streamsim.py $TMPJSONFILE
+    echo -n $users "" >> $OUT_FILE
+    cat $TMPCOSTFILE | $SCRIPT | tr -d '\n' >> $OUT_FILE
+    echo -n " " >> $OUT_FILE
+    cat $TMPQOSFILE | $SCRIPT | tr -d '\n'>> $OUT_FILE
+    echo -n " " >> $OUT_FILE
+    cat $TMPSESSFILE | $SCRIPT | tr -d '\n'>> $OUT_FILE
+    echo >> $OUT_FILE
+rm -f $TMPJSONFILE $TMPCOSTFILE $TMPDAYFILE $TMPQOSFILE    
+done
+
+OUT_FILE="data/poker_var_user_nrandom.out"
+IN_FILE="scripts/poker_var_user_nrandom.json"
+
+rm -f $OUT_FILE
+for users in "2500" "5000" "7500" "10000"; do
+    sed -e "s/XXXXXX/$users/g" $IN_FILE | sed -e "s%DAYTMPFILE%$TMPDAYFILE%g" | sed -e "s%COSTTMPFILE%$TMPCOSTFILE%g" | sed -e "s%QOSTMPFILE%$TMPQOSFILE%g" | sed -e "s%SESSTMPFILE%$TMPSESSFILE%g"> $TMPJSONFILE
+    src/streamsim.py $TMPJSONFILE
+    echo -n $users "" >> $OUT_FILE
+    cat $TMPCOSTFILE | $SCRIPT | tr -d '\n' >> $OUT_FILE
+    echo -n " " >> $OUT_FILE
+    cat $TMPQOSFILE | $SCRIPT | tr -d '\n'>> $OUT_FILE
+    echo -n " " >> $OUT_FILE
+    cat $TMPSESSFILE | $SCRIPT | tr -d '\n'>> $OUT_FILE
+    echo >> $OUT_FILE
+rm -f $TMPJSONFILE $TMPCOSTFILE $TMPDAYFILE $TMPQOSFILE    
+done
